d88743e4cf7854824befe4b0a42ace4a95f8f055
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-05-08 20:03:07 -0500

    Add progressively collapsing menu

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const menubar = document.querySelector('.menubar')
-const ul = document.querySelector('ul')
-const li = document.querySelector('li')
+const more = document.querySelector('.more-wrapper')
+const menuItems = document.querySelector('.menu-items')
+const menuItem = document.querySelector('.menu-item')
+const moreItems = document.querySelector('.more-items')
+const moreItem = document.querySelector('.more-item')
 
-menubar.addEventListener('click', () => {
-    ul.classList.toggle('pop')
+menubar.addEventListener('mouseover', () => {
+    menuItems.classList.add('pop')
+})
+
+menubar.addEventListener('mouseout', () => {
+    menuItems.classList.remove('pop')
+})
+
+more.addEventListener('mouseover', () => {
+    moreItems.classList.add('pop')
+})
+
+more.addEventListener('mouseout', () => {
+    moreItems.classList.remove('pop')
 })
