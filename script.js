65ed0179fccd79899f3a78c9d2ff04cd68d07b8d
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-05-09 20:10:57 -0500

    Slides through images using Navigation circles

--- script.js
+++ script.js
@@ -24,17 +24,19 @@ more.addEventListener('mouseout', () => {
 const leftArrow = document.querySelector('.left-arrow')
 const rightArrow = document.querySelector('.right-arrow')
 const photos = document.querySelector('.photos')
+const dots = document.querySelector('.dots')
+let currentSlide
+let nextSlide
+let index
 
 leftArrow.addEventListener('click', () => {
-    
-    let currentSlide
-    let nextSlide
     
     for (let i = 0; i < photos.children.length;i++){
         
         if (photos.children[i].classList.contains('show')) {
             currentSlide = photos.children[i]
-            nextSlide = photos.children[i-1]
+            nextSlide = photos.children[i - 1]
+            index = i-1
         }
     }
     
@@ -44,19 +46,24 @@ leftArrow.addEventListener('click', () => {
     }else {
         currentSlide.classList.remove('show')
         photos.lastElementChild.classList.add('show')
+        index = 4
     }
+
+    for (let i = 0; i < dots.children.length; i++){
+        dots.children[i].firstElementChild.classList.remove('active')
+    }
+
+    dots.children[index].firstElementChild.classList.add('active')
 })
 
 rightArrow.addEventListener('click', () => {
-    
-    let currentSlide
-    let nextSlide
     
     for (let i = 0; i < photos.children.length;i++){
         
         if (photos.children[i].classList.contains('show')) {
             currentSlide = photos.children[i]
-            nextSlide = photos.children[i+1]
+            nextSlide = photos.children[i + 1]
+            index = i+1
         }
     }
 
@@ -66,5 +73,33 @@ rightArrow.addEventListener('click', () => {
     } else {
         currentSlide.classList.remove('show')
         photos.firstElementChild.classList.add('show')
+        index = 0
+    }
+
+    for (let i = 0; i < dots.children.length; i++){
+        dots.children[i].firstElementChild.classList.remove('active')
+    }
+
+    dots.children[index].firstElementChild.classList.add('active')
+})
+
+dots.addEventListener('click', (event) => {
+    if (event.target.classList.contains('dot')){
+        
+        let targetDiv = event.target.closest('div')
+
+        index = Array.from(targetDiv.parentNode.children).indexOf(targetDiv)
+        
+        for (let i = 0; i < photos.children.length;i++){
+           photos.children[i].classList.remove('show')
+        }
+
+        photos.children[index].classList.add('show')
+
+        for (let i = 0; i < dots.children.length; i++){
+            dots.children[i].firstElementChild.classList.remove('active')
+        }
+
+        dots.children[index].firstElementChild.classList.add('active')
     }
 })
